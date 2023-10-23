82178fed719a933f16e1e4999dd049c31aa970d8
IT21068478 <dharanesegar08@gmail.com>
2023-10-23 18:18:37 +0530

    Added comments

--- server/controllers/ProjectController.mjs
+++ server/controllers/ProjectController.mjs
@@ -1,32 +1,51 @@
-import Project from '../models/Project.mjs';
+// Import the 'Project' model and 'logger' utility from the specified paths.
+import Project from "../models/Project.mjs";
+import logger from "../utils/logger.mjs";
 
+// Define an asynchronous function to add a project to the database.
 export const addProject = async (req, res) => {
-    const { empId, projectName } = req.body;
+  // Destructure 'empId' and 'projectName' from the request body.
+  const { empId, projectName } = req.body;
 
-    try {
-        const project = await Project.create({
-            empId,
-            projectName
-        });
-        console.log({ status: 'Success', project });
-        res.json(project);
-    } catch (err) {
-        console.log({ status: 'Error', err });
-    }
-}
+  try {
+    // Create a new project in the database with the provided 'empId' and 'projectName'.
+    const project = await Project.create({
+      empId,
+      projectName,
+    });
 
+    // Log a success message indicating that the project was created successfully and respond with the project data in JSON format.
+    console.log({ status: "Success", project });
+    res.json(project);
+    logger.info("Project created successfully");
+  } catch (err) {
+    // If an error occurs during project creation, log the error and respond with an error message.
+    console.log({ status: "Error", err });
+    logger.error(`Error creating project: ${err.message}`);
+  }
+};
+
+// Define an asynchronous function to retrieve projects by 'empId'.
 export const getProjectByEmpId = async (req, res) => {
-    const { empId } = req.params;
+  // Destructure 'empId' from the request parameters.
+  const { empId } = req.params;
 
-    try {
-        const projects = await Project.find({ empId: empId });
-        if (!projects || projects.length === 0) {
-            return res.json({ status: "No Projects found" });
-        } else {
-            return res.json(projects);
-        }
-    } catch (err) {
-        console.log({ status: "Error", err });
-        return res.json({ status: "Error", err });
+  try {
+    // Find projects in the database that match the provided 'empId'.
+    const projects = await Project.find({ empId: empId });
+
+    if (!projects || projects.length === 0) {
+      // If no projects are found, log a message and respond with a message indicating no projects were found.
+      logger.info("No projects for the site manager");
+      return res.json({ status: "No Projects found" });
+    } else {
+      // Log a success message indicating that projects were fetched and respond with the retrieved projects in JSON format.
+      logger.info("Projects fetched");
+      return res.json(projects);
     }
+  } catch (err) {
+    // If an error occurs during project retrieval, log the error and respond with an error message.
+    logger.error(`Error fetching projects: ${err.message}`);
+    return res.json({ status: "Error", err });
+  }
 };
--- server/routes/SiteManagerRoute.mjs
+++ server/routes/SiteManagerRoute.mjs
@@ -1,13 +1,17 @@
-import express from 'express';
+// Import the 'express' framework and the required controller functions from the specified paths.
+import express from "express";
+import { register, login } from "../controllers/SiteManagerController.mjs";
 
-import {
-  register,
-  login
-} from '../controllers/SiteManagerController.mjs';
-
+// Create an instance of an Express Router.
 const route = express.Router();
 
+// Define routes for handling site manager registration and login.
+
+// Route to register a site manager, using the 'register' controller function.
 route.post("/register", register);
+
+// Route to log in a site manager, using the 'login' controller function.
 route.post("/login", login);
 
+// Export the router to be used by the application.
 export default route;
